048746df487534111a8319ff86b4f4339b23acb5
JakubKarp <jakub.karp@innespacery.pl>
2017-12-29 13:46:30 +0100

    final

--- server.js
+++ server.js
@@ -5,38 +5,35 @@ var app = express();
 app.set('view engine', 'pug');
 app.set('views','./views');
 
-app.use('/sklep', function(req, res, next){
-    console.log('Hej, jestem wrotami do sklepu!');
-		next();
-});
 
-app.get('/', function (req, res) {
-    res.send('Wpisz w localhost słowo: sklep. I poczekaj 2 sekundy!');
-});
 
-app.get('/sklep', function (req, res) {
-   	res.setTimeout(2000, function() {  
-		res.send('Witaj w sklepie');
-	});
-});
 
-app.get('/first-template', function(req, res){
-    res.render('first-template');
-});
-
-app.get('/login', function(req, res){
+app.get('/', function(req, res){
     res.render('button', {
 		url: 'http://localhost:3000/log',
 		name: 'Kot'
-		
 	});
-	
 });
 
 app.get('/log', function(req, res){
-    res.render('logi');
+    res.render('logi', {
+		placeholder: ' wpisz imię',
+	});
+});
+
+app.use('/afterlog', function(req, res, next){
+    console.log('Hej, wpisałeś imię: ' + req.query.login_name + '!');
+		next();
 	
 });
+
+app.get('/afterlog', function(req, res){
+    res.render('voila', {
+		your_name: req.query.login_name		   
+	});
+});
+
+
 
 
 app.listen(3000);
